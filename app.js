70f0ac3fd9d335fa8e680abb2c22445745bc70b5
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-25 20:10:01 +0800

    feat: add boilerplate and packages

---
+++ app.js
@@ -0,0 +1,11 @@
+// Server Initialization
+const express = require('express');
+const app = express();
+app.listen(8000, () => {
+  console.log(`Now listening on http://localhost:8000`);
+});
+// Server Settings
+app.set('view engine', 'ejs');
+// Routers
+const indexRouter = require('./routes/index.js');
+app.use(indexRouter);
